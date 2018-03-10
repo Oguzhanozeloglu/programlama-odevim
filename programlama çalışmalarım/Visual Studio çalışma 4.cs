0db713181d1ce12a022b94b60e02021724622249
Oguzhanozeloglu <37240797+Oguzhanozeloglu@users.noreply.github.com>
2018-03-10 13:47:09 +0200

    ödevlerim'in devamı

---
+++ programlama çalışmalarım/Visual Studio çalışma 4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cinsiyet;
+            int yas;
+            Console.Write("lütfen cinsiyetinizi giriniz (E/K):");
+            cinsiyet=Convert.ToChar(Console.ReadLine());
+            Console.Write("Lütfen yaşınızı giriniz:");
+            yas = Convert.ToInt32(Console.ReadLine());
+                if (((cinsiyet == 'E') || (cinsiyet == 'e')) && (yas >= 20))
+                {
+                    Console.WriteLine("Askere gidebilir");
+                }
+                else
+                {
+                    Console.WriteLine("Askere gidemez");
+                }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ programlama çalışmalarım/Visual Studio çalışma 6249.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme123456789
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı1 = 6;
+            int sayı2 = 5;
+            int sayı3 = 11;
+            if(sayı1==sayı2)
+            {
+             Console.WriteLine("sayı 1 ile sayı 2 birbirine eşit");
+            }
+            else if (sayı1+sayı2 == sayı3)
+            {
+                Console.WriteLine("sayı 1 ile 2 toplamı sayı 3 e eşit ");
+            }
+            else
+            { Console.WriteLine("Hiç eşitlik yok");
+            }
+            Console.ReadKey();
+
+
+            }
+    }
+}
