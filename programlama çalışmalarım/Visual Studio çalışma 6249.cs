aae681bf44a89186e9ff72d7e9ed196c1e84cc3f
Gökhan Özeloğlu <33967642+gozeloglu@users.noreply.github.com>
2018-03-10 15:07:49 +0300

    Bazı yazım yanlışları ve parantezler düzeltildi.

--- programlama çalışmalarım/Visual Studio çalışma 6249.cs
+++ programlama çalışmalarım/Visual Studio çalışma 6249.cs
@@ -13,20 +13,16 @@ namespace Deneme123456789
             int sayı1 = 6;
             int sayı2 = 5;
             int sayı3 = 11;
-            if(sayı1==sayı2)
-            {
-             Console.WriteLine("sayı 1 ile sayı 2 birbirine eşit");
+            if(sayı1==sayı2){
+                Console.WriteLine("sayı 1 ile sayı 2 birbirine eşit");
             }
-            else if (sayı1+sayı2 == sayı3)
-            {
+            else if (sayı1+sayı2 == sayı3){
                 Console.WriteLine("sayı 1 ile 2 toplamı sayı 3 e eşit ");
             }
-            else
-            { Console.WriteLine("Hiç eşitlik yok");
+            else{
+                Console.WriteLine("Hiç eşitlik yok");
             }
             Console.ReadKey();
-
-
             }
     }
 }
