d0debd52c5ec4b040387e0fcfcb7396601924d93
Oguzhanozeloglu <37240797+Oguzhanozeloglu@users.noreply.github.com>
2018-03-10 15:32:05 +0300

    Bazı düzeltmeler yapıldı

---
+++ programlama çalışmalarım/VisualStudio3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+
+            Console.WriteLine("Sayı 1'i Giriniz..: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sayı 2'yi  Giriniz..: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+                Console.WriteLine("Sayı 1 Büyük");
+            else
+                Console.WriteLine("Sayı 2 Büyük");
+            Console.ReadKey();
+        }
+    }
+}
